92a46f5a3f945e76107deebbea512c116868550e
ledoanminhnhat <95356540+ledoanminhnhat@users.noreply.github.com>
2024-04-23 14:38:51 +0700

    GICUNGDC

---
+++ User.java
@@ -0,0 +1,53 @@
+public class User {
+    // Attributes
+    private String userID;
+    private String password;
+    private String email;
+    private String userType;
+
+    // Constructor
+    public User(String userID, String password, String email, String userType) {
+        this.userID = userID;
+        this.password = password;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    // Operations
+    public void resetPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    // Getters and Setters
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
