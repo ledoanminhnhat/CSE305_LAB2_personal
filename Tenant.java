38375bf462e192c418a478a7d1f791028bc15afa
ledoanminhnhat <95356540+ledoanminhnhat@users.noreply.github.com>
2024-04-23 16:09:04 +0700

    update

---
+++ Tenant.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rental_management;
+
+import java.util.*;
+
+/**
+ *
+ * @author kelyt
+ */
+public class Tenant {
+
+    User user;
+    private static List<User> listOfContract = new ArrayList<>();
+
+    public static void requestCreateRentalContract() {
+    }
+
+    public static void requestTerminateRentalContract() {
+    }
+}
